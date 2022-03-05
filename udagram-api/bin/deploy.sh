ff552dd530a0686850786e49dabfdc17d3b47e7e
KhaliedCodes <63527474+KhaliedCodes@users.noreply.github.com>
2022-03-05 02:30:50 +0200

    edits

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -2,4 +2,4 @@ eb init --platform node.js --region us-east-1
 eb create --sample udagram-api-dev
 eb use udagram-api-dev
 eb deploy udagram-api-dev
-eb setenv POSTGRES_HOST=database-2.cvfuprjd4wtz.us-east-1.rds.amazonaws.com POSTGRES_DB=postgres POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=postgres DB_PORT=5432 JWT_SECRET=thisissosecret
+eb setenv PORT=$PORT POSTGRES_HOST=$POSTGRES_HOST POSTGRES_DB=$POSTGRES_DB POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD DB_PORT=$DB_PORT JWT_SECRET=$JWT_SECRET
